c433558dfc8cf4b8761e6773672c1c1b9d41dfa8
J0onYEong <cjy2134@naver.com>
2023-07-14 21:30:56 +0900

    Initial Commit

---
+++ Project-Helper/Project_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_HelperApp.swift
+//  Project-Helper
+//
+//  Created by 최준영 on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct Project_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
